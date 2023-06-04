867540c94e666c793c2205707e5d913f901cf823
Ihsan <mihsan6133@gmail.com>
2023-06-04 19:26:00 +0400

    First Commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+
+/// Module for easy manipulation of ntfs usn journals / change journals in windows.
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
